838a9c31d1e05992ffda10a1a2d6c4542db86937
Joao Vicente <112505296+joaorovera@users.noreply.github.com>
2024-07-13 14:13:42 -0300

    colocando os arquivos que foram finalizados
    Os arquivos colocados fazem parte do final do projeto, que foi finalizado.

--- log.php
+++ log.php
@@ -1,22 +1,23 @@
 <?php
-
+session_start();
 include_once 'conectar.php';
 
-// Verifica se os dados foram submetidos via POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
-    // Obtém os dados do formulário
     $email = $_POST["email"];
     $senha = $_POST["password"];
 
     // Consulta SQL para verificar se o email e a senha correspondem a um usuário no banco de dados
-    $sql = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
-    $resultado = $conexao->query($sql);
+    $sql = "SELECT id_usuario FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+    $resultado = $conn->query($sql);
 
-    // Verifica se a consulta retornou algum resultado
+    
+
     if ($resultado->num_rows > 0) {
-        // Login bem-sucedido
-        header("Location: paginicial.html");
+        $row = $resultado->fetch_assoc();
+        $_SESSION['id_usuario'] = $row['id_usuario']; // Armazena o id_usuario na sessão
+
+        header("Location: paginicial.php");
         exit();
     } else {
         // Login falhou
